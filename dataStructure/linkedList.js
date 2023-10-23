7d3712f23dca916251a27d604574a7d7a9d150c9
dott <hyeongman24@gmail.com>
2023-10-23 09:46:43 +0900

    feat: 링크드 리스트 push 메서드 추가

--- dataStructure/linkedList.js
+++ dataStructure/linkedList.js
@@ -18,16 +18,23 @@ class SinglyLinkedList {
     if (!this.head) {
       this.head = node;
       this.tail = this.head;
+      this.length++;
+      return;
     }
 
-    this.head.next = node;
+    this.tail.next = node;
     this.tail = node;
+    this.length++;
   }
 }
 
 const list = new SinglyLinkedList();
 
 list.push(1);
-console.log(list.head.value);
+console.log(list.length);
 list.push(2);
-console.log(list.head.value);
+console.log(list.head.next.value);
+list.push(3);
+console.log(list.head.next.next.value);
+console.log(list.length);
+console.log(list.tail.value);
