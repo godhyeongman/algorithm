3d1a52acbb1fa37f8ec2c6726de855723c8b1f7d
dott <hyeongman24@gmail.com>
2023-10-31 11:19:19 +0900

    feat: double 링크드 리스트 shift 메서드 구현

--- dataStructure/doubleLinkedlist.js
+++ dataStructure/doubleLinkedlist.js
@@ -40,6 +40,8 @@ class DoubleLinkedList {
     if (this.head === this.tail) {
       this.tail = null;
       this.head = null;
+      this.length = 0;
+
       return currentTail;
     }
 
@@ -51,9 +53,37 @@ class DoubleLinkedList {
     currentTail.prev = null;
     return currentTail;
   }
+
+  shift() {
+    if (this.head === null) {
+      return;
+    }
+
+    const currentHead = this.head;
+
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+
+      return currentHead;
+    }
+
+    this.head = this.head.next;
+    this.head.prev = null;
+
+    this.length--;
+
+    currentHead.next = null;
+    return currentHead;
+  }
 }
 
 const list = new DoubleLinkedList();
+
+list.push(1);
+
+list.shift();
 
 console.log(list.head);
 console.log(list.tail);
