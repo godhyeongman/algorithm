0c8ddbce41deaef991924eb241f63f909e995d4c
dott <hyeongman24@gmail.com>
2023-10-31 10:26:45 +0900

    feat: double 링크드 리스트 push 구현

--- dataStructure/doubleLinkedlist.js
+++ dataStructure/doubleLinkedlist.js
@@ -12,4 +12,29 @@ class DoubleLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(value) {
+    const node = new Node(value);
+
+    if (this.head === null) {
+      this.head = node;
+      this.tail = this.head;
+      return this;
+    }
+
+    this.tail.next = node;
+    node.prev = this.tail;
+    this.tail = node;
+    this.length++;
+
+    return this;
+  }
 }
+
+const list = new DoubleLinkedList();
+
+list.push(1);
+list.push(2);
+
+console.log(list.head.next.value);
+console.log(list.tail.value);
