9eb67c543858ad01638147f4cbb58aca854ab060
dott <aaaa4000@naver.com>
2024-10-23 15:50:11 +0900

    feat: linkedList pop, shift

--- dataStructure/Lls.js
+++ dataStructure/Lls.js
@@ -3,11 +3,6 @@ class Node {
     this.value = props;
     this.next = null;
   }
-
-  push(value) {
-    this.next = new Node(value);
-    return this.next;
-  }
 }
 
 class LinkedList {
@@ -25,16 +20,59 @@ class LinkedList {
       this.length++;
 
       return this;
-      return;
     }
 
     this.tail.next = new Node(value);
-    return this.tail;
+    this.tail = this.tail.next;
+    return this;
+  }
+
+  pop() {
+    let current = this.head;
+    let prev = null;
+    if (!current) return;
+
+    while (current) {
+      if (!current.next) {
+        this.length--;
+        if (this.length === 0) {
+          this.tail = null;
+          this.head = null;
+          return current;
+        }
+
+        this.tail = prev;
+        return prev;
+      }
+      prev = current;
+      current = current.next;
+    }
+  }
+
+  shift() {
+    if (!this.head) return;
+
+    if (this.tail === this.head) {
+      this.head = null;
+      this.tail = null;
+      this.length--;
+      return this.head;
+    }
+
+    const temp = this.head;
+    this.head = this.head.next;
+    this.length--;
+
+    return temp;
   }
 }
 
 const lls = new LinkedList();
 
-lls.push(1).push(2);
+lls.push(1).push(2).push(3);
+// const popped = lls.pop();
+const shifited = lls.shift();
 
-console.log(lls.head.next.value);
+console.log(shifited.value, lls.head.value);
+
+// console.log(lls.tail.value);
