30437fc6fa01e704f8f27a33a75c0b018e08a39e
dott <aaaa4000@naver.com>
2025-10-16 16:54:35 +0900

    디큐 브라켓 페어 검증

---
+++ algorithm/validBracket.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+
+def validBracket(str):
+    dq = deque(str)
+    strDict = {"(": 0, "{": 0, "[": 0}
+
+    while len(dq) > 0:
+        poped = dq.popleft()
+
+        if poped == "(":
+            strDict["("] += 1
+        if poped == ")":
+            strDict["("] -= 1
+            if strDict["("] < 0:
+                return False
+
+        if poped == "{":
+            strDict["{"] += 1
+        if poped == "}":
+            strDict["{"] -= 1
+            if strDict["{"] < 0:
+                return False
+
+        if poped == "[":
+            strDict["["] += 1
+        if poped == "]":
+            strDict["["] -= 1
+            if strDict["["] < 0:
+                return False
+
+    for key in strDict.keys():
+        if strDict[key] > 0:
+            return False
+    return True
+
+
+print(validBracket("[{}[{)(}]]"))
