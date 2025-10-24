199254a282b8fa0749a4ccadf3a4c79327ffed04
dott <aaaa4000@naver.com>
2025-10-24 16:12:45 +0900

    큰숫자 문제 리팩토링

--- algorithm/biggerNumSearch.js
+++ algorithm/biggerNumSearch.js
@@ -1,34 +1,15 @@
 const biggerNumSearch = (nums) => {
   const stack = [];
   const numsWithDay = nums.map((val, idx) => ({ day: idx, value: val }));
-  const answer = Array.from({ length: nums.length }, () => undefined);
+  const answer = Array.from({ length: nums.length }, () => 0);
 
-  let idx = 0;
   for ({ day, value } of numsWithDay) {
-    if (idx === 0) {
-      stack.push({ day, value });
-      idx++;
-      continue;
-    }
-
-    let last = stack[stack.length - 1];
-
-    while (last.value < value) {
+    while (stack.length > 0 && stack[stack.length - 1].value < value) {
       const poped = stack.pop();
-      last = stack[stack.length - 1];
-
-      if (!poped) break;
-      answer[poped.day] = idx - poped.day;
+      answer[poped.day] = day - poped.day;
     }
-
     stack.push({ day, value });
-    idx++;
   }
-
-  answer[answer.length - 1] = 0;
-  answer.forEach((val, idx) => {
-    if (val === undefined) answer[idx] = 0;
-  });
 
   return answer;
 };
